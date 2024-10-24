addb85235d931aa1ea135afbeabbbaa9ace8c195
BBaguss <113284072+BBguss@users.noreply.github.com>
2024-10-24 13:38:08 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,24 +1,24 @@
+Import java.util.Scanner;
 public class Nomor1{
     public static void main(String[] args){
-    int tunjangan =0, jumlahAggota = 0; 
-    int tunjanganPerjiwa = 100000;
+    double tunjangan =0; 
+    int jumlahAggota; 
+    double tunjanganPerjiwa = 100000;
     int sisaAnggota=0;
     Scanner input= new Scanner(System.in);
 
-
+    jumlahAggota=0;
     System.out.println("Jumlah Anggota Keluarga: "+ jumlahAggota);
     jumlahAggota= input.nextInt();
 
-    if (jumlahAggota == 3) {
+    if (jumlahAggota <= 3) {
         tunjangan = jumlahAggota * tunjanganPerjiwa;
     }
-    else if (jumlahAggota >= 3) {
-        tunjangan = (jumlahAggota * tunjanganPerjiwa)+(sisaAnggota*500000);   
+    else if (jumlahAggota > 3) {
+        sisaAnggota =jumlahAggota -= 3 ;
+        tunjangan= (sisaAnggota*50000)+(tunjanganPerjiwa*3);
     }
-    else {
-        tunjangan = jumlahAggota*tunjanganPerjiwa;
-    }
-    System.out.println("Jumlah Anggota Keluarga: "+ jumlahAggota);
+    System.out.println("Jumlah Anggota Keluarga: ");
     System.out.println("Besar tunjangan Kesehatan: "+ tunjangan);
     }
 }
