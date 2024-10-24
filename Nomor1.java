3ae89594ad86f9d7d9826f3b9de7495f4c076018
BBaguss <113284072+BBguss@users.noreply.github.com>
2024-10-24 13:20:36 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,24 @@
 public class Nomor1{
     public static void main(String[] args){
+    int tunjangan =0, jumlahAggota = 0; 
+    int tunjanganPerjiwa = 100000;
+    int sisaAnggota=0;
+    Scanner input= new Scanner(System.in);
 
+
+    System.out.println("Jumlah Anggota Keluarga: "+ jumlahAggota);
+    jumlahAggota= input.nextInt();
+
+    if (jumlahAggota == 3) {
+        tunjangan = jumlahAggota * tunjanganPerjiwa;
+    }
+    else if (jumlahAggota >= 3) {
+        tunjangan = (jumlahAggota * tunjanganPerjiwa)+(sisaAnggota*500000);   
+    }
+    else {
+        tunjangan = jumlahAggota*tunjanganPerjiwa;
+    }
+    System.out.println("Jumlah Anggota Keluarga: "+ jumlahAggota);
+    System.out.println("Besar tunjangan Kesehatan: "+ tunjangan);
     }
 }
